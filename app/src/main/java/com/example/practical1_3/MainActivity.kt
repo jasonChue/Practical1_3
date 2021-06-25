54c77960e0b77965508a30447d7d58d461ad3fd8
JasonChue <chuejw-wm18@student.tarc.edu.my>
2021-06-25 14:31:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/practical1_3/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.practical1_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollbtn : Button = findViewById(R.id.roll_btn)
+        rollbtn.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice(){
+
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
